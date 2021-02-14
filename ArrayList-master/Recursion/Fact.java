4833a01a7033835df8944675d89b8bde70b3b5a2
Abrar051 <shahriar1410051@gmail.com>
2021-02-14 16:30:22 +0600

    new update working put method

---
+++ ArrayList-master/Recursion/Fact.java
@@ -0,0 +1,16 @@
+package Recursion;
+
+public class Fact {
+    public int factorial (int n)
+    {
+        if (n==0 || n==1 )
+        {
+            return 1;
+        }
+        else
+        {
+            return n*factorial(n-1);
+        }
+        //return n;
+    }
+}
--- ArrayList-master/Recursion/Main.java
+++ ArrayList-master/Recursion/Main.java
@@ -2,6 +2,7 @@ package Recursion;
 public class Main {
     public static void main (String args[])
     {
-
+        Fact obj = new Fact();
+        System.out.println(obj.factorial(5));
     }
 }
