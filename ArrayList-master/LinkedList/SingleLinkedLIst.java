58a417bc86d30012f16301afc56d39d84d015645
Abrar051 <shahriar1410051@gmail.com>
2021-03-01 17:19:17 +0600

    new update working put method

--- ArrayList-master/LinkedList/SingleLinkedLIst.java
+++ ArrayList-master/LinkedList/SingleLinkedLIst.java
@@ -5,40 +5,33 @@ import java.util.Arrays;
 
 public class SingleLinkedLIst {
     int data;
-    int position=1;
-    int length=1;
+    int position = 1;
+    int length = 1;
     public Node head = null;
     public Node tail = null;
     ArrayList obj = new ArrayList();
-    public void addNode (int data){
+
+    public void addNode(int data) {
         Node newNode = new Node(data);
-        if (head==null)
-        {
+        if (head == null) {
             head = newNode;
-        }
-        else
-        {
+        } else {
             tail.next = newNode;
         }
         tail = newNode;
         length++;
     }
-    public void forwardDisplay ()
-    {
+
+    public void forwardDisplay() {
         Node current = head;
-        if (head==null)
-        {
+        if (head == null) {
             System.out.println("List is empty");
             return;
-        }
-        else
-        {
-            for (int i=0;i<position ; i++)
-            {
-                current=current.next;
-                if (i==(position-1))
-                {
-                    System.out.print(current.data+" ");
+        } else {
+            for (int i = 0; i < position; i++) {
+                current = current.next;
+                if (i == (position - 1)) {
+                    System.out.print(current.data + " ");
                 }
             }
             System.out.println();
@@ -48,50 +41,43 @@ public class SingleLinkedLIst {
         this.position++;
 
     }
-    public void backwardDisplay ()
-    {
+
+    public void backwardDisplay() {
         this.position--;
         Node current = head;
-        if (head==null)
-        {
+        if (head == null) {
             System.out.println("List is empty");
             return;
-        }
-        else
-        {
-            for (int i=0;i<position ; i++)
-            {
-                current=current.next;
-                if (i==(position-1))
-                {
-                    System.out.print(current.data+" ");
+        } else {
+            for (int i = 0; i < position; i++) {
+                current = current.next;
+                if (i == (position - 1)) {
+                    System.out.print(current.data + " ");
                 }
             }
             System.out.println();
         }
 
-
-
+    }
+    public void backDisplay ()
+    {
 
     }
-    public void display ()
-    {
+    public void display() {
         Node current = head;
-        if (head==null)
-        {
+        if (head == null) {
             System.out.println("List is empty");
             return;
         }
         System.out.println("nodes of singly linked list : ");
-        while (current!=null)
-        {
-            System.out.print(current.data+" ");
-            current=current.next;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
         }
         System.out.println();
     }
-    Node reverse(Node node)
-    {
+
+    Node reverse(Node node) {
         Node prev = null;
         Node current = node;
         Node next = null;
@@ -106,29 +92,11 @@ public class SingleLinkedLIst {
     }
 
     // prints content of double linked list
-    public void reverseDisplay(Node node)
-    {
+    public void reverseDisplay(Node node) {
         reverse(head);
         while (node != null) {
             System.out.print(node.data + " ");
             node = node.next;
         }
     }
-
-
-   /* public void reverseDisplay ()
-    {
-        Node prev = null;
-        Node current=head;
-        Node next = null;
-        while (current!=null)
-        {
-            next=current.next;
-            current.next=prev;
-            prev=current;
-            current=next;
-        }
-        head=prev;
-        System.out.print(Arrays.toString(new Node[]{head}));
-    }*/
 }
